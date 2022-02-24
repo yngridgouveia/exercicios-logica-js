d801992a39c128688f27f3568fcb031aef7b184e
yngridgouveia <yngridgouveiakdvc@gmail.com>
2022-02-24 19:53:30 -0300

    exercicio_017

---
+++ 017/script.js
@@ -0,0 +1,13 @@
+//fça um programa que peça dois números e imprima o maior deles.
+let n1,n2
+n=10
+n2=13
+n3=24
+if(n1 > n2 && n2>n3){
+    console.log("o maior número é:",n1)
+}
+if(n2>n1 && n1>n3){
+    console.log("o maior número é:",n2)
+}else{
+    console.log("o maior número é:",n3)
+}
